7c0db90ba350468b8627f99d3d2786e594c10712
Victoria Tu <134049149+victoriatwo@users.noreply.github.com>
2023-06-21 16:20:34 -0700

    its updated

---
+++ lab1.js
@@ -0,0 +1,13 @@
+// Start with population n llamas. Goal is f llamas.
+// Each year, n/3 llamas are added and n/4 llamas are subtracted
+function totalYears(currentPopulation, targetPopulation) {
+    let years = 0;
+    while (currentPopulation < targetPopulation) {
+        currentPopulation = currentPopulation + Math.trunc(currentPopulation/3) - Math.trunc(currentPopulation/4);
+        years++;
+    }
+    // Return years until currentPopulation reaches targetPopulation
+    return years;
+}
+
+console.log(totalYears(100, 1000000));
