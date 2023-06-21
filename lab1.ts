ea0a1fba985203ee551685ded87986163f5cc2f9
Victoria Tu <134049149+victoriatwo@users.noreply.github.com>
2023-06-21 16:26:10 -0700

    changed to typescript :(

--- lab1.ts
+++ lab1.ts
@@ -1,6 +1,6 @@
 // Start with population n llamas. Goal is f llamas.
 // Each year, n/3 llamas are added and n/4 llamas are subtracted
-function totalYears(currentPopulation, targetPopulation) {
+function totalYears(currentPopulation: number, targetPopulation: number) {
     let years = 0;
     while (currentPopulation < targetPopulation) {
         currentPopulation = currentPopulation + Math.trunc(currentPopulation/3) - Math.trunc(currentPopulation/4);
